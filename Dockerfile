0857a13ff63930028f03cfd5a7dcaa3ad0c2c573
paul <paul@wizmacau.com>
2016-05-16 11:50:01 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -3,18 +3,11 @@ FROM node
 WORKDIR /parse
 
 ADD package.json /parse
-ADD index.js /parse
 
 RUN npm install
 
-ENV APP_ID=appId
-ENV MASTER_KEY=masterKey
-
 ENV ADMIN_USER=user
 ENV ADMIN_PASS=pass
-
-ENV PARSE_PUBLIC_ADDR=localhost
-ENV PARSE_PUBLIC_PORT=1337
 
 ENV PARSE_DASHBOARD_ALLOW_INSECURE_HTTP=true
 
--- index.js
+++ index.js
@@ -6,10 +6,16 @@ var app = express();
 var dashboard = new ParseDashboard({
     "apps": [
         {
-            "serverURL": "http://" + process.env.PARSE_PUBLIC_ADDR + ":" + process.env.PARSE_PUBLIC_PORT + "/parse",
-            "appId": process.env.APP_ID,
-            "masterKey": process.env.MASTER_KEY, // Keep this key secret!
-            "appName": "demo"
+            "serverURL": "http://" + process.env.S1_ENV_PARSE_PUBLIC_ADDR + ":" + process.env.S1_ENV_PARSE_PUBLIC_PORT + "/parse",
+            "appId": process.env.S1_ENV_APP_ID,
+            "masterKey": process.env.S1_ENV_MASTER_KEY, // Keep this key secret!
+            "appName": process.env.S1_ENV_APP_NAME || "s1-demo"
+        },
+        {
+            "serverURL": "http://" + process.env.S2_ENV_PARSE_PUBLIC_ADDR + ":" + process.env.S2_ENV_PARSE_PUBLIC_PORT + "/parse",
+            "appId": process.env.S2_ENV_APP_ID,
+            "masterKey": process.env.S2_ENV_MASTER_KEY, // Keep this key secret!
+            "appName": process.env.S2_ENV_APP_NAME || "s2-demo"
         }
     ],
     "users": [
